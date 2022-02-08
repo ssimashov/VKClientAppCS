fcb836c8507dde397f2f28e968d97945be0ef538
Sergey Simashov <simashov@gmail.com>
2022-02-08 14:42:24 +0300

    Added Singleton Session

---
+++ VKClientApp/Models/SingletonSession.swift
@@ -0,0 +1,18 @@
+//
+//  SingletonSession.swift
+//  VKClientAppCS
+//
+//  Created by Sergey Simashov on 08.02.2022.
+//
+
+import Foundation
+
+final class Session {
+    
+    static let instance = Session()
+    
+    private init() {}
+    
+    var token: String = ""
+    var userId: Int = 0
+}
