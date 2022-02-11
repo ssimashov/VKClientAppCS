0901e0f64c9178833a9793b08cb39a402a6b8f2a
Sergey Simashov <simashov@gmail.com>
2022-02-12 00:24:37 +0300

    Added Webview and Unwins segue

--- VKClientApp/VC/Login/VKLoginViewController.swift
+++ VKClientApp/VC/Login/VKLoginViewController.swift
@@ -16,6 +16,25 @@ final class VKLoginViewController: UIViewController {
         }
     }
     
+    @IBAction func unwindToVKLogin(_ segue: UIStoryboardSegue) {
+        Session.instance.token = ""
+        Session.instance.userId = 0
+        let dataStore = WKWebsiteDataStore.default()
+        dataStore.fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+            records.forEach{
+                if $0.displayName.contains("vk"){
+                    dataStore.removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), for: [$0]) {[weak self] in
+                        guard
+                            let self = self,
+                            let url = self.urlComponents.url
+                        else { return }
+                        self.webView.load(URLRequest(url: url))
+                    }
+                }
+            }
+        }
+    }
+    
     private var urlComponents: URLComponents = {
         var comp = URLComponents()
         comp.scheme = "https"
@@ -44,5 +63,44 @@ final class VKLoginViewController: UIViewController {
 
 
 extension VKLoginViewController: WKNavigationDelegate {
+    func webView(
+        _ webView: WKWebView,
+        decidePolicyFor navigationResponse: WKNavigationResponse,
+        decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard
+            let url = navigationResponse.response.url,
+            url.path == "/blank.html",
+            let fragment = url.fragment
+            else {
+               return decisionHandler(.allow)
+            }
+            
+            let params = fragment
+                      .components(separatedBy: "&")
+                      .map { $0.components(separatedBy: "=") }
+                      .reduce([String: String]()) { result, param in
+                          var dict = result
+                          let key = param[0]
+                          let value = param[1]
+                          dict[key] = value
+                          return dict
+                  }
+            
+            guard
+                let token = params["access_token"],
+                let userIDString = params["user_id"],
+                let userID = Int(userIDString)
+            else { return decisionHandler(.allow) }
+            
+            Session.instance.token = token
+            Session.instance.userId = userID
+            
+            performSegue(
+                withIdentifier: "moveToLogin",
+                sender: nil)
+            
+            decisionHandler(.cancel)
+            
+        }
     
 }
